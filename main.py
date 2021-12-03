e7cb7d1c40ff76ba00578919d1dc0edf28de6a21
DmitriyLesnoy <vasymedov@gmail.com>
2021-12-04 00:29:10 +1000

    2

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
 from simple_intervals import simple_intervals
+from tonality import tonality
 
 intervals=simple_intervals()
+tonalnost=tonality()
 
 note=['','c']
 
-print(note)
-print(intervals.small_secunda(note[0],note[1]),' - small secunda')
-print(intervals.big_secunda(note[0],note[1]),' - big secunda')
-print(intervals.small_tercia(note[0],note[1]),' - small tercia')
-print(intervals.big_tercia(note[0],note[1]),' - big tercia')
-print(intervals.free_cvarta(note[0],note[1]),' - free cvarta')
-print(intervals.free_cvinta(note[0],note[1]),' - free cvinta')
-print(intervals.small_sexta(note[0],note[1]),' - small sexta')
-print(intervals.big_sexta(note[0],note[1]),' - big sexta')
-print(intervals.small_septima(note[0],note[1]),' - small septima')
-print(intervals.big_septima(note[0],note[1]),' - big septima')
-print(intervals.octava(note[0],note[1]),' - octava')
+
+
---
+++ tonality.py
@@ -0,0 +1,16 @@
+from _typeshed import Self
+
+
+class tonality():
+    def __init__(self):
+        pass
+    
+    def cis_dur():
+
+        print('a')
+
+
+
+abc=tonality()
+
+abs.cisdur()
